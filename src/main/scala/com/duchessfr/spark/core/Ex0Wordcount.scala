bdcad95478596b0f5062cd9379af5f38255b4c19
Ludwine Probst <ludwine.probst@gmail.com>
2015-06-20 21:05:33 +0200

    add wordcount

--- src/main/scala/com/duchessfr/spark/core/Ex0Wordcount.scala
+++ src/main/scala/com/duchessfr/spark/core/Ex0Wordcount.scala
@@ -7,32 +7,50 @@ import org.apache.spark.rdd.RDD
 import com.duchessfr.spark.utils._
 
 /**
- *  Count how much each word appears in a file and make some operation on the result
+ *  The Java Spark API documentation: http://spark.apache.org/docs/latest/api/java/index.html
+ *
+ *  Here the goal is to count how much each word appears in a file and make some operation on the result.
+ *  We use the mapreduce pattern to do this:
+ *
+ *  step 1, the mapper:
+ *  - we attribute 1 to each word. And we obtain then couples (word, 1), where word is the key.
+ *
+ *  step 2, the reducer:
+ *  - for each key (=word), the values are added and we will obtain the total amount.
+ *
  */
 object Ex0Wordcount extends App {
-  
-  
-    //This is a toy exercice to get you to know some spark basics
-    // use the spark shell for this.
 
-    val pathToFile = "data/reduced-tweets.txt"
+    val pathToFile = "data/wordcount.txt"
 
- 
+    def loadData(): RDD[String] = {
+      // create spark configuration and spark context
+      val conf = new SparkConf()
+                          .setAppName("Wordcount")
+                          .setMaster("local[*]")
 
-    // create spark  configuration and spark context
-    val conf = new SparkConf()
-                    .setAppName("Wordcount")
-                    .setMaster("local[*]")
+      val sc = new SparkContext(conf)
 
-    val sc = new SparkContext(conf)
+      // load data and create an RDD of string, and count each word count
+      sc.textFile(pathToFile).flatMap(line => line.split(" "))
 
-    // load data and create an RDD of string, and count each word count
-    val tweets = sc.textFile(pathToFile)
-                   .flatMap( line => line.split(" "))
-                   .map (word => (word, 1))
-                   .reduceByKey( (a,b) => a+b)
-            
-    tweets.top(10)((Ordering.by(m => m._2)))
-          .foreach(println)
+    }
+
+    def wordcount(): RDD[(String, Int)] = {
+      val tweets = loadData()
+
+      // mapper step at first and then the reducer step
+      tweets.map (word => (word, 1))
+            .reduceByKey( (a,b) => a+b)
+
+    }
+
+
+    def filterOnWordcount(): RDD[(String, Int)] = {
+      val tweets = wordcount()
+
+      tweets.filter(couple => couple._2 > 4)
+
+    }
 
 }
